72c8d5fef98769940578acf1917815290c7eb273
Majorasu <46532169+Majorasu@users.noreply.github.com>
2023-04-24 22:05:46 +0200

    1.1 and 1.2

--- OpenGL_Praktikum/src/Game/Scene.cpp
+++ OpenGL_Praktikum/src/Game/Scene.cpp
@@ -35,6 +35,30 @@ bool Scene::init()
 		 */
 
 
+        glGenBuffers(1, &vboID); //generate VBO
+        glBindBuffer(GL_ARRAY_BUFFER,vboID); //Activate Buffer
+        glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),&vertices,GL_STATIC_DRAW); //upload Data to vram
+
+        glGenVertexArrays(1,&vaoID);
+        glBindVertexArray(vaoID);
+
+        //VAO Attribute Verticies
+        glVertexAttribPointer(0, 2, GL_FLOAT, false, 5*4, 0); //4 Byte one Float
+        glEnableVertexAttribArray(0);
+        //VAO Attribute Colors
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 5*4, (void*)8); //8 offset cause color begins at 9th byte
+        glEnableVertexAttribArray(1);
+
+        GLuint iboID = 0;
+        glGenBuffers(1, &iboID);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,iboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices), indices, GL_STATIC_DRAW);
+
+        //Unbind
+        /*
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); */
 
         std::cout << "Scene initialization done\n";
         return true;
@@ -54,6 +78,9 @@ void Scene::render(float dt)
     * Place your code here!
     * ************
     */
+    glBindVertexArray(vaoID); //activate VAO and implicit VBO
+    glDrawElements(GL_TRIANGLES, 9,GL_UNSIGNED_INT,0);
+    glBindVertexArray(0);
 
 
 
