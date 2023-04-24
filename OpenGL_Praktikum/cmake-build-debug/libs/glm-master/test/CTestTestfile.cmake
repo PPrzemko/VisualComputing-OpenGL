9a777d013d7612b94efbc18dd7ac42a3f48fd9c7
Majorasu <46532169+Majorasu@users.noreply.github.com>
2023-04-24 21:11:09 +0200

    init

---
+++ OpenGL_Praktikum/cmake-build-debug/libs/glm-master/test/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: D:/!Git/Studium/Semester4/VC/Praktikum1/OpenGL_Praktikum/libs/glm-master/test
+# Build directory: D:/!Git/Studium/Semester4/VC/Praktikum1/OpenGL_Praktikum/cmake-build-debug/libs/glm-master/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("bug")
+subdirs("core")
+subdirs("ext")
+subdirs("gtc")
+subdirs("gtx")
